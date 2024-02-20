3e73c39d2d1fb5be03b4a73ea735a567eb919190
zlatansmail <66694454+zlatansmail@users.noreply.github.com>
2024-02-20 22:03:34 +0100

    added components in atomic design principle

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import navigation from '/components/Navigation.js'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='navigation'>
+          <div className='nav-list'>
+            <li className='nav-item'>Vijesti</li>
+            <li className='nav-item'>Biznis</li>
+            <li className='nav-item'>Sport</li>
+            <li className='nav-item'>Magazin</li>
+            <li className='nav-item'>Lifestyle</li>
+            <li className='nav-item'>Scitech</li>
+            <li className='nav-item'>Auto</li>
+            <li className='nav-item'>Križaljka</li>
+            <li className='nav-item'>Posao</li>
+            <li className='nav-item'>Forum</li>
+          </div>
+        </div>
       </header>
     </div>
   );
---
+++ src/components/Navigation.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+function Navigation(props) {
+    return <h1>Hello, {props.name}</h1>;
+  }
