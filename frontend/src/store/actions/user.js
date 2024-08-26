0171a30a9f1edf5df6a938243b9662a74d517024
Zlatan Smailebgovic <zlatan.s@outlook.com>
2024-08-26 13:03:46 +0200

    Moved backend to another branch

--- frontend/src/store/actions/user.js
+++
@@ -1,6 +0,0 @@
-import { userActions } from "../reducers/userReducers.js";
-
-export const logout = () => (dispatch) => {
-  dispatch(userActions.resetUserInfo());
-  localStorage.removeItem("account");
-}
